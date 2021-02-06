b5ae3a4e12bf74c166a709249e15f83230859ead
Shaikh Rehaan <37113935+srrahmaan@users.noreply.github.com>
2021-02-06 23:43:53 +0530

    Project and Contact Section Added

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("solutionTab1").('click', function () {
+       
+   })
+});
+
+ function activaTab(tab){
+    $('.nav-tabs a[href="#' + tab + '"]').tab('show');
+};
